a5ff75ec3990d4f5395bd6ebad1991702ba62c6a
Shadee Merhi <shadmerhi@gmail.com>
2021-08-02 11:55:50 -0700

    add extra test quote to test selectedQuote

--- server/database/seeders/20210801070403-quotes.js
+++ server/database/seeders/20210801070403-quotes.js
@@ -1,19 +1,37 @@
 'use strict';
 module.exports = {
 	up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('Quotes', [{
-      departure_location: 'Vancouver',
-      destination_location: 'Calgary',
-	  departure_datetime: new Date('August 9, 2021 03:30:00'),
-	  destination_datetime: new Date('August 9, 2021 04:55:00'),
-      travellers: 4,
-      transporation_type: 'Rental Car',
-      name: 'Shadee Merhi',
-      price: 377.77,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }], {});
-    /**
+		await queryInterface.bulkInsert(
+			'Quotes',
+			[
+				{
+					departure_location: 'Vancouver',
+					destination_location: 'Calgary',
+					departure_datetime: new Date('August 9, 2021 03:30:00'),
+					destination_datetime: new Date('August 9, 2021 04:55:00'),
+					travellers: 4,
+					transporation_type: 'Rental Car',
+					name: 'Shadee Merhi',
+					price: 377.77,
+					createdAt: new Date(),
+					updatedAt: new Date()
+				},
+				{
+					departure_location: 'Las Vegas',
+					destination_location: 'Montreal',
+					departure_datetime: new Date('November 12, 2021 13:37:00'),
+					destination_datetime: new Date('November 12, 2021 15:55:00'),
+					travellers: 4,
+					transporation_type: 'Rental Car',
+					name: 'Emily Jones',
+					price: 1201.22,
+					createdAt: new Date(),
+					updatedAt: new Date()
+				}
+			],
+			{}
+		);
+		/**
 		 * Add seed commands here.
 		 *
 		 * Example:
@@ -28,9 +46,9 @@ module.exports = {
 		/**
 		 * Add commands to revert seed here.
 		 *
-     * Example:
+		 * Example:
 		 * await queryInterface.bulkDelete('People', null, {});
 		 */
-    await queryInterface.bulkDelete('Quotes', null, {});
+		await queryInterface.bulkDelete('Quotes', null, {});
 	}
 };
