b46e0f010845995c23ba6ffa5cd92001a38945b2
Shadee Merhi <shadmerhi@gmail.com>
2021-08-02 22:40:50 -0700

    add post route for quotes, insert into db

--- server/controllers/quoteController.js
+++ server/controllers/quoteController.js
@@ -4,3 +4,8 @@ exports.getAllQuotes = async (req, res) => {
     const quotes = await db.Quote.findAll();
     return quotes;
 }
+
+exports.createQuote = async (req, res) => {
+    const { quote } = req.body;
+    return await db.Quote.create(quote);
+}
--- server/routes/quotes.js
+++ server/routes/quotes.js
@@ -1,11 +1,18 @@
 const router = require('express').Router();
-
-const db = require('../database/models');
 const quoteController = require('../controllers/quoteController');
 
 router.get('/', async (req, res) => {
-    const quotes = await quoteController.getAllQuotes();
-    res.json({ quotes });
+	const quotes = await quoteController.getAllQuotes();
+	res.json({ quotes });
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const quote = await quoteController.createQuote(req);
+        res.status(200).json({ quote });
+    } catch (thing) {
+        res.json({ error: 'Error creating quote' });
+    }
 });
 
 module.exports = router;
