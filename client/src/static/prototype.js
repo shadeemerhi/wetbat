7db9288ec7ab1e557cbc1e017bf8187b29080a24
Shadee Merhi <shadmerhi@gmail.com>
2021-08-02 19:45:26 -0700

    create static data

---
+++ client/src/static/prototype.js
@@ -0,0 +1,64 @@
+export const DEFAULT_FORM = {
+	departureLocation: null,
+	departureDate: new Date(),
+	destinationLocation: null,
+	returnDate: new Date(),
+	numberOfTravellers: null,
+	clientName: '',
+	clientEmail: '',
+	transportationType: ''
+};
+
+export const prototypeCities = [
+	{
+		title: 'Vancouver'
+	},
+	{
+		title: 'Calgary'
+	},
+	{
+		title: 'Montreal'
+	},
+	{
+		title: 'Toronto'
+	},
+	{
+		title: 'Edmonton'
+	},
+	{
+		title: 'Ottawa'
+	},
+	{
+		title: 'Mississauga'
+	},
+	{
+		title: 'Winnipeg'
+	},
+	{
+		title: 'Québec'
+	},
+	{
+		title: 'Hamilton'
+	},
+	{
+		title: 'Brampton'
+	},
+	{
+		title: 'Surrey'
+	},
+	{
+		title: 'Kelowna'
+	},
+	{
+		title: 'Victoria'
+	}
+];
+
+export const prototypeTransportationTypes = [
+	{
+		title: 'None'
+	},
+	{
+		title: 'Rental Car'
+	}
+];
