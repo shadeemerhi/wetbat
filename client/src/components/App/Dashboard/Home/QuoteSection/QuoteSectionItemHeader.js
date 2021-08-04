e2af7b24bc8789c66d4bf226e44e77f1c86426a1
Shadee Merhi <shadmerhi@gmail.com>
2021-08-03 22:45:10 -0700

    add delete icon to header

--- client/src/components/App/Dashboard/Home/QuoteSection/QuoteSectionItemHeader.js
+++ client/src/components/App/Dashboard/Home/QuoteSection/QuoteSectionItemHeader.js
@@ -1,10 +1,28 @@
+import { useContext } from 'react';
+
+// Contexts
+import { QuoteContext } from '../../../../../contexts/QuoteContext';
+
+// Component libraries
+import DeleteIcon from '@material-ui/icons/Delete';
+
 import './QuoteSectionItemHeader.css';
 
-const QuoteSectionItemHeader = (props) => (
-        <div className="QuoteSectionItemHeader">
-            <props.icon className="Icon" />
-            <h3 className="Text">{props.headerText}</h3>
-        </div>
-);
+const QuoteSectionItemHeader = props => {
+	const {
+		quoteState: { selectedQuote }
+	} = useContext(QuoteContext);
+	return (
+		<div className="QuoteSectionItemHeader">
+			<div className="TitleContainer">
+				<props.icon className="Icon" />
+				<h3 className="Text">{props.headerText}</h3>
+			</div>
+			{props.headerText === 'Quote Details' && selectedQuote.id && (
+				<DeleteIcon onClick={() => props.setOpen(true)} className="DeleteIcon" />
+			)}
+		</div>
+	);
+};
 
 export default QuoteSectionItemHeader;
